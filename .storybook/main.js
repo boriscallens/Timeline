7cab861c436831dff3a9f3e494399a6f39f571ca
Boris Callens <boris.callens+github@gmail.com>
2020-07-22 15:30:23 +0200

    added storybookjsd

---
+++ .storybook/main.js
@@ -0,0 +1,5 @@
+module.exports = {
+  stories: ['../src/**/*.stories.ts'],
+  addons: ['@storybook/addon-links', '@storybook/addon-notes'],
+  // addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-notes'],
+};
---
+++ src/app/timeline/timeline.stories.ts
@@ -0,0 +1,26 @@
+import { TimelineComponent } from './timeline.component';
+import { IPhase } from './phase.model';
+import { IMilestone } from './milestone.model';
+
+export default {
+  title: 'Timeline',
+  component: TimelineComponent,
+};
+
+// Watch out! Months are the only datepart that is 0 based :O
+export const withText = () => ({
+  component: TimelineComponent,
+  props: {
+    Phases : [
+      {name: 'registration', startUTC: Date.UTC(2019, 10, 12), endUTC: Date.UTC(2020, 3, 6)},
+      {name: 'decision', startUTC: Date.UTC(2020, 1, 27), endUTC: Date.UTC(2020, 3, 6)},
+      {name: 'hidden week', startUTC: Date.UTC(2020, 2, 28), endUTC: Date.UTC(2020, 3, 6)},
+    ] as IPhase[],
+    Milestones: [
+      {name: 'auction', dateUTC: Date.UTC(2020, 1, 11)},
+      {name: 'personal offer', dateUTC: Date.UTC(2020, 1, 28)},
+      {name: 'registration extension 1', dateUTC: Date.UTC(2020, 1, 10)},
+      {name: 'registration extension 2', dateUTC: Date.UTC(2020, 2, 28)},
+    ] as IMilestone[]
+  },
+});
